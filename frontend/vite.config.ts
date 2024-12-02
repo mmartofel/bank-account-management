5fc3cfaa4451f874aa356e884b21fdf9526326ff
mmartofel <mmartofe@redhat.com>
2024-12-02 15:12:44 +0100

    refactor frontend proxy api calls to backend 2

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -18,24 +18,24 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy) => {
           proxy.on('error', (err) => {
             console.log('proxy error', err)
           })
           proxy.on('proxyReq', (_proxyReq, req) => {
-            console.log('Sending Request to the Target:', req.method, req.url)
+            console.log('Sending Request:', req.method, req.url)
           })
           proxy.on('proxyRes', (proxyRes, req) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+            console.log('Received Response:', proxyRes.statusCode, req.url)
           })
         },
-      },
+      }
     },
   },
   preview: {
