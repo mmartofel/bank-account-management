b05874bed6defd04f5402b0037c754531f7da9ab
mmartofel <mmartofe@redhat.com>
2024-12-02 11:22:48 +0100

    refactor API communication 1

--- frontend/src/services/transactionService.ts
+++ frontend/src/services/transactionService.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { Transaction, TransactionFilters, PaginatedResponse } from '../types/transaction';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "http://backend:8080/api";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
